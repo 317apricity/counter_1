6b520498cd92406703f7ec6f6279a7a884c999a4
317apricity <moonjw4682@naver.com>
2026-06-17 16:38:47 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { initializeFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// Firebase 구성 정보 설정
 const firebaseConfig = {
     apiKey: "AIzaSyCFYgoTpp0tfv3Md-a6qMIlJ-4Fc0EHVNQ",
     authDomain: "maechuri-68bb6.firebaseapp.com",
@@ -13,6 +12,11 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+// 🚀 보안 프로그램의 차단을 우회하기 위해 롱폴링 통신을 강제 설정합니다.
+const db = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+    useFetchStreams: false
+});
 
 export { db, doc, getDoc, setDoc };
